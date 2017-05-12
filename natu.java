79116329d7b77d5725caa4a95b7d2954d6fd837e
sowbhakya <sowbhakya0295@gmail.com>
2017-05-12 22:34:24 +0530

    Create natu.java

---
+++ natu.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class prog6
+{
+public static void main(String args[]) throws IOException
+{
+int n=100;
+int m;
+m=n*(n+1)/2;
+System.out.println("The sum of natural number is:"+m);
+}
+}
