d4fb16daf5d75a806188008fbb016281aa8d5b52
sowbhakya <sowbhakya0295@gmail.com>
2017-05-14 22:57:29 +0530

    Create oddnumber.java

---
+++ oddnumber.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class prog9
+{
+public static void main(String args[]) throws IOException
+{
+int num,m =20,n=40;
+System.out.println("Print Odd Numbers in a given range m to n:\n");
+for (num = m; num <= n; num++)
+{
+ if (num % 2 == 1)
+System.out.println(+num);
+}
+}
+}
