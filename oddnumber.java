1b26742ed1688f784407785c0c93680e5ea9f00e
sowbhakya <sowbhakya0295@gmail.com>
2017-05-14 23:00:58 +0530

    Update oddnumber.java

--- oddnumber.java
+++ oddnumber.java
@@ -4,7 +4,7 @@ public class prog9
 public static void main(String args[]) throws IOException
 {
 int num,m =20,n=40;
-System.out.println("Print Odd Numbers in a given range m to n:\n");
+System.out.println("Print Odd Numbers in a given range" +m+ "to" +n);
 for (num = m; num <= n; num++)
 {
  if (num % 2 == 1)
@@ -12,3 +12,5 @@ System.out.println(+num);
 }
 }
 }
+
+         
